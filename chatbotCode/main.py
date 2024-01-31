5cfb5d5e26e39e40f32f7e1b9edd39e81ef10e77
wjmellon <94713257+wjmellon@users.noreply.github.com>
2024-01-30 22:05:52 -0700

    shorten context output to enable more read in

--- chatbotCode/main.py
+++ chatbotCode/main.py
@@ -1,16 +1,63 @@
-# This is a sample Python script.
+import subprocess
+from collections import deque
+import os
+print(os.environ['OPENAI_API_KEY'])
+API_KEY = os.environ['OPENAI_API_KEY']
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Define the maximum number of exchanges (question + answer pairs) to keep
+MAX_EXCHANGES = 4
+
+# Initialize a deque to store the exchanges with a maximum length
+assistant_messages = deque(maxlen=MAX_EXCHANGES * 2)
+
+import autogen
+import os
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+from autogen.agentchat.contrib.retrieve_assistant_agent import RetrieveAssistantAgent
+from autogen.agentchat.contrib.retrieve_user_proxy_agent import RetrieveUserProxyAgent
+from transformers import GPT2Tokenizer
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+
+gpt_config_list = [
+    {
+        "model": "gpt-4", #can use gpt-4 too
+        "api_key": API_KEY,
+    }
+]
+
+llm_config = {"config_list": gpt_config_list,"temperature": 0}
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+assistant = RetrieveAssistantAgent(
+    name="assistant",
+    system_message="You are a helpful assistant answering questions about Acral Lentiginous Melanoma in a clinical setting. When you answer questions refer to the context given and provide citation from context.",
+    llm_config=llm_config,
+)
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+recur_spliter = RecursiveCharacterTextSplitter(separators=["\n", "\r", "\t"])
+
+ragproxyagent = RetrieveUserProxyAgent(
+    name="ragproxyagent",
+    retrieve_config={
+        "task": "qa",
+        "docs_path": "https://raw.githubusercontent.com/wjmellon/aidiagnostics/main/aggregated.txt",
+        "custom_text_split_function": recur_spliter.split_text,
+    },
+)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Before sending the context to the model
+problem = input("Ask a question about Acral Lentiginous Melanoma: ")
+assistant_messages.append(problem)  # Add the new question to the context
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Reset the assistant before each new interaction
+assistant.reset()
+
+# If you need to process the messages (e.g., tokenize or concatenate)
+context = " ".join(list(assistant_messages))  # concatenate all messages in the deque
+
+# Pass the context to the model
+ragproxyagent.initiate_chat(assistant, problem=context)
+
